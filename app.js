a2e64842aa8e356c744b90ea3908a2b609f21551
Tavi <tavioltean@yahoo.com>
2022-03-06 16:46:10 +0200

    Making 2 players work

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+
+let numberSwitch = 1;
+
 document.querySelectorAll(".box").forEach(box => {
     box.addEventListener("click", () => {
-        box.classList.add("square");
+        if(numberSwitch % 2 == 0) {
+            if(!box.classList.contains("square") && !box.classList.contains("circle")) {
+                box.classList.add("square");
+                numberSwitch++;
+            }
+        }
+
+        else {
+            if(!box.classList.contains("square") && !box.classList.contains("circle")) {
+                box.classList.add("circle");
+                numberSwitch++;
+            }
+        }
     })
 });
-
-
-
