8dd6450d1b5babdb74cdf12e14343e70670c79eb
Tavi <tavioltean@yahoo.com>
2022-03-25 14:18:31 +0200

    Give'em proper names & reset score button

--- app.js
+++ app.js
@@ -1,89 +1,97 @@
 winbar = document.querySelector(".winbar");
 let numberSwitch = 1;
-let playerX = 0;
-let player0 = 0;
-let elemBy = [];
+let playerXscore = 0;
+let player0score = 0;
+let tilesContainer = [];
 
-let test1;
-let test2;
-let test3;
-let test4;
-let test5;
-let test6;
-let test7;
-let test8;
-let test9;
+let tile1;
+let tile2;
+let tile3;
+let tile4;
+let tile5;
+let tile6;
+let tile7;
+let tile8;
+let tile9;
 
 function tablePlay() {
     document.querySelectorAll(".box").forEach(box => {
-        elemBy.push(box);
-        test1 = elemBy[0];
-        test2 = elemBy[1];
-        test3 = elemBy[2];
-        test4 = elemBy[3];
-        test5 = elemBy[4];
-        test6 = elemBy[5];
-        test7 = elemBy[6];
-        test8 = elemBy[7];
-        test9 = elemBy[8];
+        tilesContainer.push(box);
+        tile1 = tilesContainer[0];
+        tile2 = tilesContainer[1];
+        tile3 = tilesContainer[2];
+        tile4 = tilesContainer[3];
+        tile5 = tilesContainer[4];
+        tile6 = tilesContainer[5];
+        tile7 = tilesContainer[6];
+        tile8 = tilesContainer[7];
+        tile9 = tilesContainer[8];
         
         box.addEventListener("click", () => {
             if(numberSwitch % 2 == 0) {
-                if(!box.classList.contains("square") && !box.classList.contains("circle")) {
-                    box.classList.add("square");
+                if(!box.classList.contains("playerX") && !box.classList.contains("player0")) {
+                    box.classList.add("playerX");
                     numberSwitch++;
 
-                    if((test1.classList.contains("square") && test2.classList.contains("square") && test3.classList.contains("square")) || (test4.classList.contains("square") && test5.classList.contains("square") && test6.classList.contains("square"))
-                    || (test7.classList.contains("square") && test8.classList.contains("square") && test9.classList.contains("square")) || (test1.classList.contains("square") && test4.classList.contains("square") && test7.classList.contains("square"))
-                    || (test2.classList.contains("square") && test5.classList.contains("square") && test8.classList.contains("square")) || (test3.classList.contains("square") && test6.classList.contains("square") && test9.classList.contains("square"))
-                    || (test1.classList.contains("square") && test5.classList.contains("square") && test9.classList.contains("square")) || (test3.classList.contains("square") && test5.classList.contains("square") && test7.classList.contains("square"))) {
+                    if((tile1.classList.contains("playerX") && tile2.classList.contains("playerX") && tile3.classList.contains("playerX")) || (tile4.classList.contains("playerX") && tile5.classList.contains("playerX") && tile6.classList.contains("playerX"))
+                    || (tile7.classList.contains("playerX") && tile8.classList.contains("playerX") && tile9.classList.contains("playerX")) || (tile1.classList.contains("playerX") && tile4.classList.contains("playerX") && tile7.classList.contains("playerX"))
+                    || (tile2.classList.contains("playerX") && tile5.classList.contains("playerX") && tile8.classList.contains("playerX")) || (tile3.classList.contains("playerX") && tile6.classList.contains("playerX") && tile9.classList.contains("playerX"))
+                    || (tile1.classList.contains("playerX") && tile5.classList.contains("playerX") && tile9.classList.contains("playerX")) || (tile3.classList.contains("playerX") && tile5.classList.contains("playerX") && tile7.classList.contains("playerX"))) {
                         winbar.classList.remove("hidden");
                         winbar.classList.add("playerXwon");                      
-                        playerX++;
-                        document.querySelector(".player").innerHTML = "Player X won!";
-                        document.querySelector("#playerX").innerHTML = `Player <span>X</span> wins: ${playerX}`;
+                        playerXscore++;
+                        document.querySelector(".showWinner").innerHTML = "Player X won!";
+                        document.querySelector("#playerXscore").innerHTML = `Player <span>X</span> wins: ${playerXscore}`;
                     }
                 
-                    else if((test1.classList.contains("square") || test1.classList.contains("circle")) && (test2.classList.contains("square") || test2.classList.contains("circle")) && (test3.classList.contains("square") || test3.classList.contains("circle")) && (test4.classList.contains("square") || test4.classList.contains("circle")) && (test5.classList.contains("square") || test5.classList.contains("circle")) && (test6.classList.contains("square") || test6.classList.contains("circle")) && (test7.classList.contains("square") || test7.classList.contains("circle")) && (test8.classList.contains("square") || test8.classList.contains("circle")) && (test9.classList.contains("square") || test9.classList.contains("circle"))) {
+                    else if((tile1.classList.contains("playerX") || tile1.classList.contains("player0")) && (tile2.classList.contains("playerX") || tile2.classList.contains("player0")) && (tile3.classList.contains("playerX") || tile3.classList.contains("player0")) && (tile4.classList.contains("playerX") || tile4.classList.contains("player0")) && (tile5.classList.contains("playerX") || tile5.classList.contains("player0")) && (tile6.classList.contains("playerX") || tile6.classList.contains("player0")) && (tile7.classList.contains("playerX") || tile7.classList.contains("player0")) && (tile8.classList.contains("playerX") || tile8.classList.contains("player0")) && (tile9.classList.contains("playerX") || tile9.classList.contains("player0"))) {
                         winbar.classList.remove("hidden");
                         winbar.classList.add("tie");
-                        document.querySelector(".player").innerHTML = "Tie!";
+                        document.querySelector(".showWinner").innerHTML = "Tie!";
                     }
                 }
             }
             else {
-                if(!box.classList.contains("square") && !box.classList.contains("circle")) {
-                    box.classList.add("circle");
+                if(!box.classList.contains("playerX") && !box.classList.contains("player0")) {
+                    box.classList.add("player0");
                     numberSwitch++;
 
-                    if((test1.classList.contains("circle") && test2.classList.contains("circle") && test3.classList.contains("circle")) || (test4.classList.contains("circle") && test5.classList.contains("circle") && test6.classList.contains("circle"))
-                    || (test7.classList.contains("circle") && test8.classList.contains("circle") && test9.classList.contains("circle")) || (test1.classList.contains("circle") && test4.classList.contains("circle") && test7.classList.contains("circle"))
-                    || (test2.classList.contains("circle") && test5.classList.contains("circle") && test8.classList.contains("circle")) || (test3.classList.contains("circle") && test6.classList.contains("circle") && test9.classList.contains("circle"))
-                    || (test1.classList.contains("circle") && test5.classList.contains("circle") && test9.classList.contains("circle")) || (test3.classList.contains("circle") && test5.classList.contains("circle") && test7.classList.contains("circle"))) {
+                    if((tile1.classList.contains("player0") && tile2.classList.contains("player0") && tile3.classList.contains("player0")) || (tile4.classList.contains("player0") && tile5.classList.contains("player0") && tile6.classList.contains("player0"))
+                    || (tile7.classList.contains("player0") && tile8.classList.contains("player0") && tile9.classList.contains("player0")) || (tile1.classList.contains("player0") && tile4.classList.contains("player0") && tile7.classList.contains("player0"))
+                    || (tile2.classList.contains("player0") && tile5.classList.contains("player0") && tile8.classList.contains("player0")) || (tile3.classList.contains("player0") && tile6.classList.contains("player0") && tile9.classList.contains("player0"))
+                    || (tile1.classList.contains("player0") && tile5.classList.contains("player0") && tile9.classList.contains("player0")) || (tile3.classList.contains("player0") && tile5.classList.contains("player0") && tile7.classList.contains("player0"))) {
                         winbar.classList.remove("hidden");
                         winbar.classList.add("player0won");               
-                        player0++;
-                        document.querySelector(".player").innerHTML = "Player 0 won!";
-                        document.querySelector("#player0").innerHTML = `Player <span>0</span> wins: ${player0}`;
+                        player0score++;
+                        document.querySelector(".showWinner").innerHTML = "Player 0 won!";
+                        document.querySelector("#player0score").innerHTML = `Player <span>0</span> wins: ${player0score}`;
                     }
                     
-                    else if((test1.classList.contains("square") || test1.classList.contains("circle")) && (test2.classList.contains("square") || test2.classList.contains("circle")) && (test3.classList.contains("square") || test3.classList.contains("circle")) && (test4.classList.contains("square") || test4.classList.contains("circle")) && (test5.classList.contains("square") || test5.classList.contains("circle")) && (test6.classList.contains("square") || test6.classList.contains("circle")) && (test7.classList.contains("square") || test7.classList.contains("circle")) && (test8.classList.contains("square") || test8.classList.contains("circle")) && (test9.classList.contains("square") || test9.classList.contains("circle"))) {
+                    else if((tile1.classList.contains("playerX") || tile1.classList.contains("player0")) && (tile2.classList.contains("playerX") || tile2.classList.contains("player0")) && (tile3.classList.contains("playerX") || tile3.classList.contains("player0")) && (tile4.classList.contains("playerX") || tile4.classList.contains("player0")) && (tile5.classList.contains("playerX") || tile5.classList.contains("player0")) && (tile6.classList.contains("playerX") || tile6.classList.contains("player0")) && (tile7.classList.contains("playerX") || tile7.classList.contains("player0")) && (tile8.classList.contains("playerX") || tile8.classList.contains("player0")) && (tile9.classList.contains("playerX") || tile9.classList.contains("player0"))) {
                         winbar.classList.remove("hidden");
                         winbar.classList.add("tie");
-                        document.querySelector(".player").innerHTML = "Tie!";
+                        document.querySelector(".showWinner").innerHTML = "Tie!";
                     }
                 }
             }
         })
 
-        let resetButton = document.querySelector(".reset");
-        resetButton.addEventListener("click", () => {
+        let resetTableButton = document.querySelector(".resettable");
+        resetTableButton.addEventListener("click", () => {
         winbar.classList.remove("player0won");
         winbar.classList.remove("playerXwon");
         winbar.classList.remove("tie");
         winbar.classList.add("hidden");
-        box.classList.remove("square");
-        box.classList.remove("circle");
+        box.classList.remove("playerX");
+        box.classList.remove("player0");
+        })
+
+        let resetScoreButton = document.querySelector(".resetscore");
+        resetScoreButton.addEventListener("click", () => {
+        player0score = 0;
+        playerXscore = 0;
+        document.querySelector("#player0score").innerHTML = `Player <span>0</span> wins: ${player0score}`;
+        document.querySelector("#playerXscore").innerHTML = `Player <span>X</span> wins: ${playerXscore}`;
         })
     })
 };
